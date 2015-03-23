cd1bc56e2d3b5776967acdfd035503b558ae5fa8
freelix <felixx.baril@gmail.com>
2015-03-23 02:19:18 -0400

    Add footer, Refactored, Adjust css to make responsive

--- syncedFiles/the3dshop/config/initializers/assets.rb
+++ syncedFiles/the3dshop/config/initializers/assets.rb
@@ -11,3 +11,6 @@ Rails.application.config.assets.version = '1.0'
 # Rails.application.config.assets.precompile += %w( search.js )
 
 Rails.application.config.assets.precompile += %w( bootstrap.min.js )
+Rails.application.config.assets.precompile += %w( style.css )
+Rails.application.config.assets.precompile += %w( footer.css )
+Rails.application.config.assets.precompile += %w( table.css )
